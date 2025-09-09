29a0880288cca8638531d1d79b8bde68733ce607
nahu02 <nahu1002@gmail.com>
2025-09-09 13:23:20 +0200

    Refactored light and movement detection

--- ex3.ino
+++ ex3.ino
@@ -3,12 +3,34 @@
 int photoSensorPin = A0;   // select the input pin for the potentiometer
 int photoSensorValue = 0;  // variable to store the value coming from the sensor
 
-int darkness = 600;
+int darknessTreshold = 600;
+float accelerationTreshold = 2.0;
 
 GY521 sensor(0x68);
 
 uint32_t counter = 0;
 
+bool lightAndMoving(float acceleration, int light) {
+  bool isMoving = acceleration > accelerationTreshold;
+  bool isDark = light < darknessTreshold;
+  
+  // char buffer[256];
+  // char acc[10];
+  // dtostrf(acceleration, 2, 2, acc);
+  // snprintf(buffer, sizeof(buffer), "Movement: %s (%s);\tLight: %s (%d)", isMoving ? "moving" : "still", acc, isDark ? "dark" : "light", light);
+  // Serial.println(buffer);
+
+  return isMoving && isDark;
+}
+
+int readCombinedAcceleration(GY521 sensor) {
+  sensor.readAccel();
+  float ax = sensor.getAccelX();
+  float ay = sensor.getAccelY();
+  float az = sensor.getAccelZ();
+
+  return (abs(ax) + abs(ay) + abs(az));
+}
 
 void setup() {
   Serial.begin(9600);
@@ -22,25 +44,18 @@ void setup() {
     Serial.println("\tCould not connect to GY521: please check the GY521 address (0x68/0x69)");
     delay(1000);
   }
-  sensor.setAccelSensitivity(0.5);  //  2g
+  sensor.reset();
 }
 
 void loop() {
-  // read the value from the sensor:
   photoSensorValue = analogRead(photoSensorPin);
+  int acc = readCombinedAcceleration(sensor);
 
-  sensor.read();
-  float ax = sensor.getAccelX();
-  float ay = sensor.getAccelY();
-  float az = sensor.getAccelZ();
-
-  float acc = abs(ax) + abs(ay) + abs(az);
+  bool shouldTurnLightOn = lightAndMoving(acc, photoSensorValue);
 
   Serial.print(counter);
-  Serial.print('\t');
-  if(acc > 2) {Serial.print("moving");} else {Serial.print("still");}
-  Serial.print('\t');
-  if(photoSensorValue < darkness) {Serial.print("dark");} else {Serial.print("light");}
+  Serial.print("\tShould the light be on?\t");
+  Serial.print(shouldTurnLightOn, BIN);
   Serial.println();
 
   counter++;
